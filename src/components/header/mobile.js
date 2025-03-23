801050383dda47d1d62e94c74628ad36eb463459
kyuserenity <kyuserenity@gmail.com>
2025-03-23 15:44:04 +0700

    refactor: update mobile header to conditionally render icons based on pathname

--- src/components/header/mobile.js
+++ src/components/header/mobile.js
@@ -1,16 +1,31 @@
-import { SearchIcon } from "lucide-react";
+"use client";
+
+import { MenuIcon, PlusSquareIcon, SearchIcon } from "lucide-react";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 export default function MobileHeader() {
+  const pathname = usePathname();
   return (
     <div className="flex items-center justify-between border-b-2 p-6 md:hidden">
       <div>
         <p className="font-kranky text-3xl font-semibold uppercase">watkan</p>
       </div>
       <div className="flex items-center gap-6">
-        <Link href={"/search"}>
-          <SearchIcon />
-        </Link>
+        {pathname.includes("profile") ? (
+          <>
+            <Link href={"/create"}>
+              <PlusSquareIcon className="h-7 w-7" />
+            </Link>
+            <Link href={"/setting"}>
+              <MenuIcon className="h-7 w-7" />
+            </Link>
+          </>
+        ) : (
+          <Link href={"/search"}>
+            <SearchIcon />
+          </Link>
+        )}
       </div>
     </div>
   );
