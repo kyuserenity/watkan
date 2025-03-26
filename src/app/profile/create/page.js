346ca5e025eb7d2a2ee1fbe1b5260bab04b6d756
kyuserenity <kyuserenity@gmail.com>
2025-03-26 10:44:36 +0700

    refactor: update file input to accept multiple image formats in ArtworkUploadPage

--- src/app/profile/create/page.js
+++ src/app/profile/create/page.js
@@ -147,8 +147,7 @@ export default function ArtworkUploadPage() {
             )}
             <input
               type="file"
-              hidden
-              accept="image/*"
+              hiddenaccept="image/jpeg, image/png, image/gif, image/webp, image/svg+xml, image/tiff"
               onChange={handleImageChange}
             />
           </label>
